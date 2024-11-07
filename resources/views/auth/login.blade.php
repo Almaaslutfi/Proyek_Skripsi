c49af75fd603fa8b8e3342d4e10a681190db14e0
Almaaslutfi <almaslutfi17@gmail.com>
2024-11-07 15:57:06 +0700

    Update 7-11-2024

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Nusa Data Prima - Halaman Login</title>
+    <title>Nusa Data Prima - Halaman Update</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
